affaca24e43fb010be791e5cd6f0285e3465de7c
Alef Bispo <alef.b.dos.santos@hotmail.com>
2020-12-19 22:08:07 -0300

    criando metodos

--- src/aplication/entites/UsedProduct.java
+++ src/aplication/entites/UsedProduct.java
@@ -1,4 +1,29 @@
 package aplication.entites;
 
-public class UsedProduct {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UsedProduct extends Product{
+    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+    Date manufactureDate;
+
+    public UsedProduct(String nome, Double price, Date manufactureDate) {
+        super(nome, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    public Date getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(Date manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    @Override
+    public String priceTag() {
+        return getNome() + " (used) "+
+                String.format("$ %.2f") +
+                "(Manufacture date: "+ manufactureDate;
+    }
 }
